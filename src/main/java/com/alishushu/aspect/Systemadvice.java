c92048bb3ac20cee2794103197a91b12a696c226
excepuio <excepuio@gmail.com>
2019-07-15 14:13:18 +0800

    add

--- src/main/java/com/alishushu/aspect/Systemadvice.java
+++ src/main/java/com/alishushu/aspect/Systemadvice.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.alishushu.bean.SysteminfoExample;
 import com.alishushu.mapper.SysteminfoMapper;
+import java.util.*；
 
 @Aspect
 public class Systemadvice {
